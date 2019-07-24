e347cc595ac2accdd902a31fefd39df26d1617be
Peter Gillis <petergillis@hotmail.com>
2019-07-24 18:32:44 +0200

    progress

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,5 +1,16 @@
 // get our elements
-const plyer = document.queryselector('.player');
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress  = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelector('.[data-skip]');
+const ranges = player.querySelector('.player__slider');
 // build out functions
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
 
 // hook up the event listeners
+video.addEventListener('click', togglePlay);
